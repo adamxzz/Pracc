b7c735e4fc335e915b9481c03fc001854b65ab6d
adamxzz <adamzaman51@gmail.com>
2024-07-06 10:14:18 +0100

    tried to fix

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -1,14 +1,21 @@
-import { MongoClient } from "mongodb";
-
+import { MongoClient } from 'mongodb';
+ 
 let db;
-
+ 
 async function connectToDb(cb) {
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vrb0nqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
-    await client.connect();
-    db = client.db('react-blog-db');
-    cb();
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.vrb0nqg.mongodb.net/?retryWrites=true&w=majority`);
+    try {
+        await client.connect();
+        db = client.db('react-blog-db');
+        console.log("Connected to the 'react-blog-db' database");
+        cb();
+    } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
+        cb(err);
+    }
 }
-
+ 
 export {
-    db, connectToDb,
+    db,
+    connectToDb,
 };
